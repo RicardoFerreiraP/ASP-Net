0b0774cfb6e8db0f4c40ff97e43d983b1a06c4a8
Ricardo Ferreira <ricardo.ferreirap@hotmail.com>
2018-07-27 08:42:23 -0300

    Criado as pastas do MVC e as Controllers de Home e Produto e iniciado a página index

---
+++ Aulas/Ecommerce/Controllers/ProdutoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ecommerce.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        // GET: Produto
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
