3dd53e2ebff62ab0c98e40ae7382922bae200ef1
Ricardo Ferreira <ricardo.ferreirap@hotmail.com>
2018-08-03 10:32:57 -0300

    Criado a camada DAO Utilizado CSS com Bootstrap Criado Layout

--- Aulas/Ecommerce/Controllers/ProdutoController.cs
+++ Aulas/Ecommerce/Controllers/ProdutoController.cs
@@ -1,4 +1,5 @@
-﻿using Ecommerce.Models;
+﻿using Ecommerce.DAL;
+using Ecommerce.Models;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -10,12 +11,11 @@ namespace Ecommerce.Controllers
 {
     public class ProdutoController : Controller
     {
-        Context context = new Context();
         // GET: Produto
         public ActionResult Index()
         {
             ViewBag.Data = DateTime.Now;
-            ViewBag.Produtos = context.Produtos.ToList();
+            ViewBag.Produtos = ProdutoDAO.RetornarProdutos();
             return View();
         }
 
@@ -35,31 +35,27 @@ namespace Ecommerce.Controllers
                 Categoria = txtCategoria
             };
 
-            context.Produtos.Add(produto);
-            context.SaveChanges();
+            ProdutoDAO.CadastrarProduto(produto);
 
             return RedirectToAction("Index", "Produto");
         }
 
         public ActionResult RemoverProduto(int id)
         {
-            Produto produto = context.Produtos.Find(id);
-            context.Produtos.Remove(produto);
-            context.SaveChanges();
+            ProdutoDAO.RemoverProduto(id);
             return RedirectToAction("Index", "Produto");
         }
 
         public ActionResult AlterarProduto(int id)
         {
-            ViewBag.Produto = context.Produtos.Find(id);
-            context.SaveChanges();
+            ViewBag.Produto = ProdutoDAO.BuscarProdutoPorId(id);
             return View();
         }
 
         [HttpPost]
         public ActionResult AlterarProduto(string txtNome, string txtDescricao, string txtPreco, string txtCategoria, int txtId)
         {
-            Produto produto = context.Produtos.Find(txtId);
+            Produto produto = ProdutoDAO.BuscarProdutoPorId(txtId);
             {
                 produto.ProdutoId = txtId;
                 produto.Nome = txtNome;
@@ -67,8 +63,7 @@ namespace Ecommerce.Controllers
                 produto.Categoria = txtCategoria;
                 produto.Preco = Convert.ToDouble(txtPreco);
 
-                context.Entry(produto).State = EntityState.Modified;
-                context.SaveChanges();
+                ProdutoDAO.AlterarProduto(produto);
 
                 return RedirectToAction("Index", "Produto");
 
---
+++ Aulas/Ecommerce/DAL/ProdutoDAO.cs
@@ -0,0 +1,42 @@
+﻿using Ecommerce.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Ecommerce.DAL
+{
+    public class ProdutoDAO
+    {
+        private static Context context = new Context();
+
+        public static List<Produto> RetornarProdutos()
+        {
+            return context.Produtos.ToList();
+        }
+
+        public static void CadastrarProduto(Produto produto)
+        {
+            context.Produtos.Add(produto);
+            context.SaveChanges();
+        }
+
+        public static Produto BuscarProdutoPorId(int id)
+        {
+            return context.Produtos.Find(id);
+        }
+
+        public static void RemoverProduto(int id)
+        {
+            context.Produtos.Remove(BuscarProdutoPorId(id));
+            context.SaveChanges();
+        }
+
+        public static void AlterarProduto(Produto produto)
+        {
+            context.Entry(produto).State = System.Data.Entity.EntityState.Modified;
+            context.SaveChanges();
+        }
+    }
+}
